0441910bc017fe9c746f60b4bfb615f58bb028e1
duckyounglee <q7y331xk@naver.com>
2021-11-09 00:21:37 +0900

    #7.2, 7.3

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,9 +1,24 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
     return res.render("join", { pageTitle: "Join"});
 };
-export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.end();
+export const postJoin = async(req, res) => {
+    const { email, username, password, password2, name, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.render("join", {pageTitle, errorMessage: "Password confirmation does not match.",})
+    }
+    if(await User.exists({ username })){
+        return res.render("join", {pageTitle, errorMessage: "This username is already taken.",})
+    }
+    if(await User.exists({ email })){
+        return res.render("join", {pageTitle, errorMessage: "This email is already taken.",})
+    }
+    await User.create({
+        email, username, password, name, location,
+    });
+    return res.redirect("/login");
 }
 export const edit = (req, res) => res.send("user edit");
 export const remove = (req, res) => res.send("user delete");
